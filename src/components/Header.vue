47a13caee45aeed1d2bdecbc5dc476742ec72d42
ApuEmdad <emdadul.apu@savethechildren.org>
2023-01-03 23:03:43 +0600

    test-commit

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+  <ButtonVue text="Add Task" color="Green" />
+</template>
+
+<script>
+import ButtonVue from "./Button.vue";
+
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+  components: {
+    ButtonVue,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :key="task.id" v-for="task in tasks" class="task">
+    <!--capturing and  directly emitting the id -->
+    <Task @delete-task="$emit('delete-task', task.id)" :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+
+export default {
+  name: "Tasks",
+  components: {
+    Task,
+  },
+  props: {
+    tasks: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    onDelete(id) {
+      console.log(id);
+    },
+  },
+  created() {
+    console.log(this.tasks);
+  },
+  emits: ["delete-task"],
+};
+</script>
+
+<style scope></style>
+$
